275d166359384ee77f672039f27d3f225be34586
BrunoRijo <bruno_rijo@hotmail.com>
2021-12-08 11:47:47 -0300

    Criação de telas, com recycler view e personalização de campos de texto e botões

---
+++ app/src/main/java/com/example/listadecontatos/AdicionarContato.kt
@@ -0,0 +1,11 @@
+package com.example.listadecontatos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdicionarContato : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adicionar_contato)
+    }
+}
